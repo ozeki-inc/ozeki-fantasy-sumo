82df7f1e343b0f4d858dcd4977a2adfb6bbd71f7
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2021-07-07 21:52:03 -0400

    basic setup

--- README.md
+++ README.md
@@ -7,10 +7,47 @@
 ## Features
 
 * Simple
-* Trustless
+* Transparent 
 * Account-free
 * Auto update results and leaderboards
 * Trading 
 
+## Flow
 
+* Leauge is created with initial team choices and pushed to chain
 
+## Components
+
+### Teams
+
+* Team = Pubkey (pk)
+* A team is a list of `N` wrestler names `wrestlers`, where `N` is a fixed global parameter.
+* A team is always accompanied by a signature `SIGN(wrestlers, bansho, day, nonce, pk)`
+* Teams belong to leagues
+* Teams can be modified by trading and dropping/picking up
+
+### Leagues 
+
+* A league is initialized by a fixed set of public keys (teams)
+* Players each jointly enter:
+    * Picks
+    * Public key
+* Once all the league initialization data is signed by all players, we create the league.
+* League ID = `HASH(HASH(picks(pk_1), pk_1), HASH(picks(pk_2), pk_2), .., )`
+* People can check listed public keys against the league ID to make sure no tampering.
+* Optional: league ID is posted to the testnet (see: bitcoinlib package)
+
+### Trading
+
+* A trade can be requested and is accepted when both parties sign a trade message.
+* Each team is updated accordingly
+
+### Drops / Pick ups
+
+* Teams can choose to drop one of their players and pick up an undrafted player.
+
+### Scoring
+  
+### Rules
+
+[Suggested rules](https://docs.google.com/document/d/1MOtX9giT3P6bUBNlyqHYC5LSrDJsSQz5UatvuD6M2f4/edit)
---
+++ app.py
@@ -0,0 +1,70 @@
+import sys
+import os
+import json
+import hashlib
+
+from flask import Flask
+from flask import request
+from flask import render_template
+
+
+app = Flask(__name__)
+
+def check_league(league_dict):
+    return True
+
+@app.route("/league_submit", methods=["POST", "GET"])
+def league_submit():
+    """
+    Read the league creation inputs and validate,
+    post to blockchain and return to home.
+    """
+    # do league sanity checks.
+    if request.method == 'POST':
+        result = request.form
+        n_players = sum([r.startswith('pk') for r in result])
+        n_wrestlers = sum([r.startswith('wres') for r in result]) // n_players
+
+        pubkeys = []
+        league_dict = {}
+        for i in range(n_players):
+
+            pubkeys.append(result[f'pk_{i}'])
+
+            wrestlers_i = [result[f'wrestler_{i}_{j}'] for j in range(n_wrestlers)]
+            wrestlers_i_sorted = sorted(wrestlers_i)
+            league_dict[result[f'pk_{i}']] = wrestlers_i_sorted
+        pass
+    if check_league(league_dict):
+        league_dict_string = json.dumps(league_dict)
+        return render_template("league_sign.html",
+                               league_dict=league_dict_string,
+                               pubkeys=pubkeys)
+    else:
+        return render_template("error.html")
+
+@app.route("/league_setup", methods=["POST", "GET"])
+def league_setup():
+    """
+    Take in the parameters for creating the league.
+    e.g. number of teams, any others.
+    """
+    if request.method == 'POST':
+        result = request.form
+        print(request.form)
+        n_players = int(result['teams'])
+        roster_size = int(result['roster'])
+
+    return render_template("league_create.html",
+                        roster_size=roster_size,
+                        n_teams=n_players)
+
+@app.route("/")
+def home():
+    # load available leagues
+    leagues = os.listdir("static/leagues")
+    print(leagues)
+    return render_template("home.html", leagues=leagues)
+
+if __name__ == "__main__":
+    app.run(debug=True)
